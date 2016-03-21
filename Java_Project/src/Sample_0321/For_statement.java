250b44564416d5bb055e7cdb8ea8fdecfdb346a7
inhye <c1105047@naver.com>
2016-03-21 21:04:51 +0900

    lalalalalalfofoofofofofoff

--- Java_Project/src/Sample_0321/For_statement.java
+++ Java_Project/src/Sample_0321/For_statement.java
@@ -1,10 +1,13 @@
 package Sample_0321;
 
-public class HelloWorld {
+public class For_statement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.
+
+		for(int i = 0 ; i < 10 ; i++){
+			System.out.println(" value : "+i);
+		}
 	}
 
 }
