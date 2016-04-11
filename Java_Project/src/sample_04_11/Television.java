7fbca41ddca9addd11ee21edda6da7dc7a6b514a
inhye <c1105047@naver.com>
2016-04-11 20:31:04 +0900

    0411-Last class before mid term

---
+++ Java_Project/src/sample_04_11/Television.java
@@ -0,0 +1,37 @@
+package sample_04_11;
+
+public class Television {
+	int channel;
+	int volume;
+	boolean onOff;
+	
+	void pr(){
+		System.out.println(channel+" "+volume+" "+onOff);
+	}
+
+	//int volumeUP(int up_data){
+	//	int vol=volume; 
+	//	vol+=up_data;
+	//	return vol;
+	//}
+	
+	void volumeUP(int data){
+		if(onOff == false){
+			System.out.println("TV Die");
+			return;//method 종료의 의미
+			}
+		
+		volume+=data;
+		//<-이것이 더 좋은 소스
+	}
+	
+	void volumeUP(){
+		if(onOff == false){
+			System.out.println("TV Die");
+			return;//method 종료의 의미
+			}
+		
+		volume+=1;
+		
+	}	
+}
---
+++ Java_Project/src/sample_04_11/TelevisionTest.java
@@ -0,0 +1,35 @@
+package sample_04_11;
+
+public class TelevisionTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Television mytv = new Television();
+		Television yourtv = new Television();
+		Television testtv = mytv;
+		
+		mytv.channel = 18;
+		mytv.volume = 20;
+		mytv.onOff = true;
+		mytv.volumeUP();
+		//mytv.onOff = false;
+		//mytv.volume = mytv.volumeUP(30);
+		//mytv.volumeUP(30);
+		//System.out.println(mytv.channel+" "+mytv.volume+" "+mytv.onOff);
+		mytv.pr();
+			
+		yourtv.channel = 18;
+		yourtv.volume = 20;
+		yourtv.onOff = true;
+		
+		//System.out.println(yourtv.channel+" "+yourtv.volume+" "+yourtv.onOff);
+		yourtv.pr();
+		mytv.channel = 231;
+		
+		//System.out.println(testtv.channel+" "+testtv.volume+" "+testtv.onOff);
+		testtv.pr();
+		//testtv = null;
+	}
+
+}
+//리모콘 만들기~!~!
