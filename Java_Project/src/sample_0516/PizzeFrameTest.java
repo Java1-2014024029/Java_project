261a4ea703244e15a939f849c3c0d9297a2626a7
inhye <c1105047@naver.com>
2016-05-16 20:38:28 +0900

    sample_0516

---
+++ Java_Project/src/sample_0516/PizzeFrameTest.java
@@ -0,0 +1,10 @@
+package sample_0516;
+
+public class PizzeFrameTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			PizzaFrame f = new PizzaFrame();
+	}
+
+}
