ebe27a82a06bd8cec96d484b91b32a518b4e35ff
inhye <c1105047@naver.com>
2016-05-30 20:58:32 +0900

    sample_0530

---
+++ Java_Project/src/sample_0530/AnonymousClassTest.java
@@ -0,0 +1,23 @@
+package sample_0530;
+
+interface RemoteControl{
+	void turnOn();
+	void turnOff();
+}
+public class AnonymousClassTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RemoteControl ac = new RemoteControl(){
+			public void turnOn(){
+				System.out.println("TV turnOn()");
+			}
+			public void turnOff(){
+				System.out.println("TV turnOff();");
+			}
+		};      
+		ac.turnOn();
+		ac.turnOff();
+	}
+}
+
